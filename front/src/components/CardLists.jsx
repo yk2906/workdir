3303cbbd932c0dbe1fdbe8b2dd7e6694612b83cd
y.kohama <yk050696@gmail.com>
2020-12-08 17:38:14 +0900

    first commit

---
+++ front/src/components/CardLists.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardList from './CardList';
+import '../assets/styles/style.css';
+
+const CardLists = () => {
+    return(
+        <div className="card-lists">
+            <CardList />
+            {/* <CardList /> */}
+        </div>
+    )
+}
+
+export default CardLists;
